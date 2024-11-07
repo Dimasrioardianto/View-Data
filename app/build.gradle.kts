fa1f549bceb795d536415889d00187486af7e51e
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-11-07 10:38:28 +0700

    Update Commit1"menampilkan daftar jurusan"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "id.ac.polbeng.ardianto.viewdata"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "id.ac.polbeng.ardianto.viewdata"
